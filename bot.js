14b7eb8f8f21b09a54366328c07e9e238c34dea1
Hank Hershberger <47288270+Aconka@users.noreply.github.com>
2019-02-25 23:04:50 -0500

    made a starting place

---
+++ bot.js
@@ -0,0 +1,28 @@
+var cowsay = require('cowsay');
+var Discord = require('discord.js');
+var auth = require('./auth.json');
+
+// Initialize Discord Bot
+var bot = new Discord.Client();
+
+bot.login(auth.token);
+
+bot.on('ready', function (evt) 
+{
+    console.log('Connected');
+});
+
+
+bot.on('message', message => 
+{
+    //insert code to do stuff
+});
+
+var cleanupFn = function cleanup() 
+{
+    console.log("Logging off");
+    bot.destroy();
+}
+
+process.on('SIGINT', cleanupFn);
+process.on('SIGTERM', cleanupFn);
